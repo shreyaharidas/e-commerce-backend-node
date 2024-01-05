992012109214fbabe45ffae97db8ec53d02e1729
Shreya haridas <shreyaharidas15@gmail.com>
2024-01-05 14:55:56 +0530

    express basics

--- server.js
+++ server.js
@@ -6,11 +6,28 @@ const sql=require("./src/services/sqltrial")
 
 const port = process.env.PORT || 3000; // Use the PORT variable from .env or default to 3000
 
+app.use(express.json());
+
 // Define a simple route
 app.get('/', (req, res) => {
-  sql.sqltrial();
-  res.send(`Hello, this is your Express app! Running on port ${port}`);
+
+  const {name, age}= req.query;
+
+  res.send(`${name},${age}`);
 });
+
+
+app.post("/contact",(req,res)=>{
+
+  const {name,phone, email}= req.body;
+
+if(!name){
+  res.status(422).json({message:"name missing"})
+}
+
+  res.status(200).json({message:`contact details for ${name} received as ${phone} and ${email}`})
+
+})
 
 // Start the server
 app.listen(port, () => {
